f0bfec6165525bd83f5717cab8a02c0b1bda3b8d
Brian Barrow <briancbarrow@gmail.com>
2022-07-21 14:37:10 -0600

    Added init function to main package.

--- deepgram.go
+++ deepgram.go
@@ -5,30 +5,30 @@ import (
 	"net/http"
 )
 
-var sdkVersion string = "0.1.0"
+var sdkVersion string = "0.1.2"
 
-type Deepgram struct {
+type deepgram struct {
 	ApiKey string
+	Host string 
+	Path string 
+}
+
+func Init(apiKey string, host string, path string) *deepgram {
+	if host == "" {
+		host = "api.deepgram.com"
+	}
+	if path == "" {
+		path = "/v1/projects"
+	}
+	return &deepgram{
+		ApiKey: apiKey,
+		Host: host,
+		Path: path,
+	}
 }
 
 func GetJson(resp *http.Response, target interface{}) error {
 	defer resp.Body.Close()
-
+	
 	return json.NewDecoder(resp.Body).Decode(target)
-}
-
-func (dg *Deepgram) Host(host string) string  {
-	if host == "" {
-		return "https://api.deepgram.com"
-	} else {
-		return host
-	}
-}
-
-func (dg *Deepgram) Path(path string) string  {
-	if path == "" {
-		return "/v1/projects"
-	} else {
-		return path
-	}
 }
