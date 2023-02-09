929dd8c12dc944e974be2e56ff91098865bd3a38
Brian Barrow <briancbarrow@gmail.com>
2023-02-09 15:23:09 -0700

    Added ToWebVTT and SecondsToTimestamp fun, formatting

--- deepgram/transcriptions.go
+++ deepgram/transcriptions.go
@@ -3,6 +3,7 @@ package deepgram
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"log"
@@ -14,9 +15,9 @@ import (
 )
 
 type PreRecordedResponse struct {
-	Request_id string      `json:"request_id"`
-	Metadata   interface{} `json:"metadata"`
-	Results    interface{} `json:"results"`
+	Request_id string   `json:"request_id"`
+	Metadata   Metadata `json:"metadata"`
+	Results    Results  `json:"results"`
 }
 
 type LiveTranscriptionOptions struct {
@@ -85,7 +86,6 @@ func (dg *Client) LiveTranscription(options LiveTranscriptionOptions) (*websocke
 		log.Fatal("dial:", err)
 	}
 	return c, resp, nil
-
 }
 
 func (dg *Client) PreRecordedFromURL(source UrlSource, options PreRecordedTranscriptionOptions) (PreRecordedResponse, error) {
@@ -129,4 +129,31 @@ func (dg *Client) PreRecordedFromURL(source UrlSource, options PreRecordedTransc
 		return result, nil
 	}
 
+}
+
+func (resp *PreRecordedResponse) ToWebVTT() (string, error) {
+	if resp.Results.Utterances == nil {
+		return "", errors.New("This function requires a transcript that was generated with the utterances feature.")
+	}
+
+	vtt := "WEBVTT\n\n"
+
+	vtt += "NOTE\nTranscription provided by Deepgram\nRequest ID: " + resp.Request_id + "\nCreated: " + resp.Metadata.Created + "\n\n"
+
+	for i, utterance := range resp.Results.Utterances {
+		utterance := utterance
+		// TODO: Create SecondsToTimestamp function
+		start := SecondsToTimestamp(utterance.Start)
+		end := SecondsToTimestamp(utterance.End)
+		vtt += fmt.Sprintf("%d\n%s --> %s\n%s\n\n", i+1, start, end, utterance.Transcript)
+	}
+
+	return vtt, nil
+}
+
+func SecondsToTimestamp(seconds float64) string {
+	hours := int(seconds / 3600)
+	minutes := int((seconds - float64(hours*3600)) / 60)
+	seconds = seconds - float64(hours*3600) - float64(minutes*60)
+	return fmt.Sprintf("%02d:%02d:%02.3f", hours, minutes, seconds)
 }
