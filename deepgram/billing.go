525494ee3d355d9ef9096d4cc95c74e20e850dcb
Brian Barrow <briancbarrow@gmail.com>
2023-04-04 12:00:50 -0600

    Added Billing endpoints

---
+++ deepgram/billing.go
@@ -0,0 +1,78 @@
+package deepgram
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+)
+
+type Balance struct {
+	Amount    float64 `json:"amount"`
+	BalanceId string  `json:"balance_id"`
+	Units     string  `json:"units"`
+	Purchase  string  `json:"purchase"`
+}
+
+type BalanceList struct {
+	Balances []Balance `json:"balances"`
+}
+
+func (dg *Client) ListBalances(projectId string) (BalanceList, error) {
+	client := new(http.Client)
+	path := fmt.Sprintf("%s/%s/balances", dg.Path, projectId)
+	u := url.URL{Scheme: "https", Host: dg.Host, Path: path}
+	req, err := http.NewRequest("GET", u.String(), nil)
+	if err != nil {
+		//Handle Error
+		log.Fatal(err)
+	}
+
+	var result BalanceList
+	res, err := client.Do(req)
+	if err != nil {
+		return result, err
+	}
+	if res.StatusCode != 200 {
+		b, _ := io.ReadAll(res.Body)
+		log.Fatal(string(b))
+	}
+	jsonErr := GetJson(res, &result)
+
+	if jsonErr != nil {
+		fmt.Printf("error getting balances from project %s: %s\n", projectId, jsonErr.Error())
+		return result, jsonErr
+	} else {
+		return result, nil
+	}
+}
+
+func (dg *Client) GetBalance(projectId string, balanceId string) (Balance, error) {
+	client := new(http.Client)
+	path := fmt.Sprintf("%s/%s/balances/%s", dg.Path, projectId, balanceId)
+	u := url.URL{Scheme: "https", Host: dg.Host, Path: path}
+	req, err := http.NewRequest("GET", u.String(), nil)
+	if err != nil {
+		//Handle Error
+		log.Fatal(err)
+	}
+
+	var result Balance
+	res, err := client.Do(req)
+	if err != nil {
+		return result, err
+	}
+	if res.StatusCode != 200 {
+		b, _ := io.ReadAll(res.Body)
+		log.Fatal(string(b))
+	}
+	jsonErr := GetJson(res, &result)
+
+	if jsonErr != nil {
+		fmt.Printf("error getting balance %s: %s\n", balanceId, jsonErr.Error())
+		return result, jsonErr
+	} else {
+		return result, nil
+	}
+}
