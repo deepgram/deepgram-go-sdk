bb12629ab0bc180d9163ce484d151ffba0ca991c
weiminw <weimin.wan@deepgram.com>
2023-06-27 17:20:58 -0400

    fix livestream link in example/liveTranscription

--- examples/liveTranscription_example.go
+++ examples/liveTranscription_example.go
@@ -17,7 +17,7 @@ func main() {
 	client := new(http.Client)
 	// IMPORTANT: Make sure you add your own API key here
 	dg := *deepgram.NewClient("YOUR_API_KEY")
-	resp, err := client.Get("http://stream.live.vc.bbcmedia.co.uk/bbc_radio_fourlw_online_nonuk")
+	resp, err := client.Get("http://stream.live.vc.bbcmedia.co.uk/bbc_world_service")
 	if err != nil {
 		log.Println("ERRROR getting stream", err)
 	}
