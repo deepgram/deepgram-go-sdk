9fd2983433bd17909881ddc0f3d4657cb948088c
David vonThenen <12752197+dvonthenen@users.noreply.github.com>
2024-05-30 14:52:59 -0700

    Expected Failure After 3 Failed Attempts

---
+++ tests/edge_cases/failed_retry/main.go
@@ -0,0 +1,59 @@
+// Copyright 2024 Deepgram SDK contributors. All Rights Reserved.
+// Use of this source code is governed by a MIT license that can be found in the LICENSE file.
+// SPDX-License-Identifier: MIT
+
+package main
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"os"
+
+	interfaces "github.com/deepgram/deepgram-go-sdk/pkg/client/interfaces"
+	client "github.com/deepgram/deepgram-go-sdk/pkg/client/live"
+)
+
+func main() {
+	// init library
+	client.InitWithDefault()
+
+	// Go context
+	ctx := context.Background()
+
+	// set the Transcription options
+	tOptions := &interfaces.LiveTranscriptionOptions{
+		Language:  "en-US",
+		Punctuate: true,
+	}
+
+	// create a Deepgram client
+	cOptions := &interfaces.ClientOptions{
+		EnableKeepAlive: true,
+		Host:            "127.0.0.1",
+	}
+
+	// use the default callback handler which just dumps all messages to the screen
+	dgClient, err := client.New(ctx, "", cOptions, tOptions, nil)
+	if err != nil {
+		fmt.Println("ERROR creating LiveClient connection:", err)
+		return
+	}
+
+	// connect the websocket to Deepgram
+	wsconn := dgClient.Connect()
+	if wsconn == nil {
+		fmt.Println("Client.Connect failed")
+		os.Exit(1)
+	}
+
+	// wait for user input to exit
+	fmt.Printf("This demonstrates using KeepAlives. Press ENTER to exit...\n")
+	input := bufio.NewScanner(os.Stdin)
+	input.Scan()
+
+	// close client
+	dgClient.Stop()
+
+	fmt.Printf("Program exiting...\n")
+}
